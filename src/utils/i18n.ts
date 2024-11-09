932d164f0bed37b20f5bb3458dcd11ea06a2963f
pedja98 <predragmirkovic1998@gmail.com>
2024-11-09 19:20:18 +0100

    dynamical language change

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,12 +1,13 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpBackend from 'i18next-http-backend'
+import { store } from '../app/store'
 
 i18n
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
-    lng: 'sr',
+    lng: store.getState().auth.language.toLowerCase() || 'sr',
     fallbackLng: 'en',
     debug: false,
     backend: {
@@ -17,6 +18,14 @@ i18n
     interpolation: {
       escapeValue: false,
     },
+  })
+  .then(() => {
+    store.subscribe(() => {
+      const newLanguage = store.getState().auth.language.toLowerCase()
+      if (newLanguage !== i18n.language) {
+        i18n.changeLanguage(newLanguage)
+      }
+    })
   })
 
 export default i18n
