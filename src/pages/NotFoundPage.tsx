07e2dc3491e33795ffb30c5113a2c5064e3fe4c2
pedja98 <predragmirkovic1998@gmail.com>
2025-07-21 17:01:18 +0200

    page not found force language

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,10 +2,13 @@ import { Typography } from '@mui/material'
 import { ButtonStyled, Root } from '../styles/common'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
+import { useAppSelector } from '../app/hooks'
+import { Languages } from '../consts/user'
 
 const NotFoundPage = () => {
   const { t } = useTranslation()
   const navigate = useNavigate()
+  const language = useAppSelector((state) => state.auth).language || Languages.SR
 
   const handleGoBack = () => {
     navigate(-1)
@@ -13,9 +16,9 @@ const NotFoundPage = () => {
 
   return (
     <Root>
-      <Typography variant='h1'>{t('general:pageNotFound')}</Typography>
+      <Typography variant='h1'>{t('general:pageNotFound', { lng: language })}</Typography>
       <ButtonStyled variant='contained' color='primary' onClick={handleGoBack} sx={{ marginTop: 2 }}>
-        {t('general:backToPreviousPage')}
+        {t('general:backToPreviousPage', { lng: language })}
       </ButtonStyled>
     </Root>
   )
